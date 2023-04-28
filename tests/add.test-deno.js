99a1af1389aa012614f5a725559d05e964b1d7b5
Yann <49924352+YTBeater@users.noreply.github.com>
2023-04-28 16:56:10 +0200

    Update add.test-deno.js

--- tests/add.test-deno.js
+++ tests/add.test-deno.js
@@ -1,9 +1,32 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assertEquals, assertThrows } from "https://deno.land/std/testing/asserts.ts";
 import { add_deno } from '../app-deno.js';
 
 Deno.test("test de la fonction add-deno", () => {
   assertEquals(add_deno(1, 2), 3);
   assertEquals(add_deno(2, -3), -1);
-  assertEquals(add_deno(0, 0), 0); // Ajout d'un autre test
-  assertEquals(add_deno(NaN, 2), NaN); // Ajout d'un autre test
+  assertEquals(add_deno(0, 0), 0);
+  
+  assertThrows(
+    () => {
+      add_deno(NaN, 2);
+    },
+    Error,
+    "Les paramètres doivent être des nombres"
+  );
+
+  assertThrows(
+    () => {
+      add_deno(2, NaN);
+    },
+    Error,
+    "Les paramètres doivent être des nombres"
+  );
+
+  assertThrows(
+    () => {
+      add_deno(NaN, NaN);
+    },
+    Error,
+    "Les paramètres doivent être des nombres"
+  );
 });
