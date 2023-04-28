19f1886ab6cf6b468ffd798c5a4234ad244b518a
Yann <49924352+YTBeater@users.noreply.github.com>
2023-04-28 16:14:58 +0200

    Update add.test-deno.js

--- tests/add.test-deno.js
+++ tests/add.test-deno.js
@@ -4,6 +4,6 @@ import { add_deno } from '../app-deno.js';
 Deno.test("test de la fonction add-deno", () => {
   assertEquals(add_deno(1, 2), 3);
   assertEquals(add_deno(2, -3), -1);
-  assertEquals(add_deno("hello", 2), NaN);
-  assertEquals(add_deno("1", "2"), NaN);
+  assertEquals(add_deno(0, 0), 0); // Ajout d'un autre test
+  assertEquals(add_deno(NaN, 2), NaN); // Ajout d'un autre test
 });
