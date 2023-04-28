a5baa3a60843c8a4e44a05435191919ac342484c
Yann <49924352+YTBeater@users.noreply.github.com>
2023-04-28 15:57:06 +0200

    Update add.test-deno.js

--- tests/add.test-deno.js
+++ tests/add.test-deno.js
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { add_deno } from '../app.js';
+import { add_deno } from '../app-deno.js';
 
 Deno.test("test de la fonction add", () => {
   assertEquals(add_deno(1, 2), 3);
