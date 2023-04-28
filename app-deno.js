b297302fe1a7dd26186064b85c5c79824f7db941
Yann <49924352+YTBeater@users.noreply.github.com>
2023-04-28 16:54:27 +0200

    Update app-deno.js

--- app-deno.js
+++ app-deno.js
@@ -1,5 +1,5 @@
 export const add_deno = (a, b) => {
-  if (isNaN(Number(a)) || isNaN(Number(b))) {
+  if (isNaN(a) || isNaN(b)) {
     throw new Error('Les paramètres doivent être des nombres');
   }
   return a + b;
